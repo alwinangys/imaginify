357a147eb04c8c8f8b70524632476c3f20dc8f83
Alwin Ang <alwinang99@gmail.com>
2024-07-03 18:54:42 +0800

    setup mongodb with connection, create mongodb models

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,37 @@
+// connect to mongoose mongodb database
+
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// cache connections: otherwise each action needs a connection, too many MongoDB connections open
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imaginify",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
