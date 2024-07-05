97a5648a8ae3916ade002cb1a7d1e5f7d826a568
Alwin Ang <alwinang99@gmail.com>
2024-07-05 16:16:29 +0800

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define routes that should be protected
-const isProtectedRoute = createRouteMatcher([
-  '/' // Add any additional routes here
-]);// Update clerkMiddleware to manually protect routes
+// // Define routes that should be protected
+// const isProtectedRoute = createRouteMatcher([
+//   '/' // Add any additional routes here
+// ]);// Update clerkMiddleware to manually protect routes
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect(); // Protect the route if it matches the defined criteria
-  }
-});
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     auth().protect(); // Protect the route if it matches the defined criteria
+//   }
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
