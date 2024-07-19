95e973d99a111cbfbd2bcca3c73b9ee3052221f4
Alwin Ang <alwinang99@gmail.com>
2024-07-19 11:16:38 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -18,6 +18,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks(.*)",
